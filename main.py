f628f977710e5eb30f03c9e7ffceca2cb5205a6e
AllThisAndThat <lucaslorenhudson@gamil.com>
2022-05-29 13:57:24 -0500

    Initial Commit.
    Can parse downloaded files for time, data, and load. Only prints to terminal.

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Author: Lucas Hudson
+Date: 5/28/22
+Goal: To learn and review python
+Task: Collect data from SPP and do something
+    Parse load data for review
+        Take every csv file from the year 2022
+            test with manually downloaded files
+            python needs to access website
+        Take the time column once. 
+        Take the OKGE column for every file
+        Compile data
+    Plot data
+    Make best fit
+
+Brainstorm
+    1. Collect data from .csv or website to plot information
+    2. Take load data and make my own best fit (even if horribly wrong)
+    3. Use wind data to make future wind production predictions
+    4. Learn fault and distances to fault calculations
+
+"""
+from parse_load_data import parse_year
+# import parse_load_data
+
+def main():
+    parse_year(2019)
+
+
+if __name__ == "__main__":
+   main()
---
+++ parse_load_data.py
@@ -0,0 +1,48 @@
+"""
+Author: Lucas Hudson
+Date: 5/28/22
+Goal: Parse .csv files
+"""
+import csv
+
+def parse_year(year):
+    times = []
+    loads = []
+    months_per_year = 12
+    for month in range(1 , months_per_year + 1):
+        parse_month(year,month,times,loads)
+    for index in range(len(times)):
+        print(times[index], ":", loads[index])
+
+        
+def parse_month(year,month,times,loads):
+    rows = []
+    FILE_NAME = '%d\HOURLY_LOAD-'%year
+    SPP_MEMBER = 'OKGE'
+
+    if month < 10:
+        FILE_NAME = '%s%d0%d.csv'%(FILE_NAME,year,month)
+    else:
+        FILE_NAME = '%s%d%d.csv'%(FILE_NAME,year,month)
+    
+    with open(FILE_NAME, mode='r') as month_data:
+        csvreader = csv.reader(month_data)
+
+        # Get top fields and strips whitespace
+        fields = next(csvreader)
+        for field in range(len(fields)):
+            fields[field] = fields[field].strip()
+            
+
+        # Format remaining data in list
+        for row in csvreader:
+            rows.append(row)
+
+        # Get times
+        for row in rows: 
+            times.append(row[0])
+
+        # Get SPP member load data
+        SPP_MEMBER_INDEX = fields.index(SPP_MEMBER)
+        for row in rows:
+            loads.append(row[SPP_MEMBER_INDEX])
